e79ce7b9aa91c6e44b5ede7c09c04c27a4666a0b
ganesh-48 <ganeshsrananavare@gmail.com>
2021-06-21 08:58:10 +0530

    refactor: address condtion was refactored

--- app/middleware/validation.js
+++ app/middleware/validation.js
@@ -3,6 +3,7 @@ const joi = require('@hapi/joi');
 const data = joi.object({
     firstName: joi.string().min(3).max(30),
     lastName: joi.string().alphanum().min(2).max(30),
+    address: joi.string().alphanum().min(2).max(30),
     city: joi.string().alphanum().min(2).max(30),
     state: joi.string().alphanum().min(2).max(30),
     phoneNumber: joi.number().min(1000000000).max(9999999999),
