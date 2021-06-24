386e6b91758d76ffe25152bcae6dc34d4a2d3cb5
ganesh-48 <ganeshsrananavare@gmail.com>
2021-06-24 09:56:56 +0530

    refactor: route file are refactored

--- app/routes/addressbookinfo.js
+++ app/routes/addressbookinfo.js
@@ -3,7 +3,13 @@ const userInfo = require('../controller/userinfo.js');
 const validate = require('../middleware/helper.js');
 
 module.exports = (app) => {
-    
+
+    // Create a new userInfo user
+    app.post('/add/userinfo', userInfo.create);
+
+    //Userlogin with a userInfoId and password
+    app.post('/userlogin', userInfo.login);
+
     // Create a new addressBookInfo
     app.post('/add', validate.checkToken, addressBookInfo.create);
 
@@ -19,9 +25,4 @@ module.exports = (app) => {
     // Delete a addressBookInfo with addressBookInfoId
     app.delete('/delete/addressBookInfo/:addressBookInfoId', validate.checkToken, addressBookInfo.deleteAddressBookInfo);
 
-    // Create a new userInfo user
-    app.post('/add/userinfo', userInfo.create);
-
-    //Userlogin with a userInfoId and password
-    app.post('/userlogin', userInfo.login);
 }
